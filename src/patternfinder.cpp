a7c0e3b499a0ecae16621a227bff30e58171b85b
Maarten van Gompel <proycon@anaproy.nl>
2011-12-05 14:24:14 +0100

    updated

--- src/patternfinder.cpp
+++ src/patternfinder.cpp
@@ -23,10 +23,10 @@ void usage() {
     cerr << "\t-c classfile     The classfile to use for decoding. If specified, decoded output will be produced" << endl;
     cerr << "\t-t <number>      Token threshold: n-grams and skipgrams occuring less than this will be pruned (default: 2)" << endl;
     cerr << "\t-l <number>      Maximum n-gram/skipgram length (in words, default: 9)" << endl;
-    cerr << "\t-s               Compute skip-grams" << endl;    
+    cerr << "\t-s               Compute skip-grams (costs extra memory and time)" << endl;    
     cerr << "\t-T <number>      Skip threshold: only skip content that occurs at least x times will be considered (default: 2) " << endl;
-    cerr << "\t-i               Compute index" << endl;
-    cerr << "\t-L               Compute content of skipgrams" << endl;
+    cerr << "\t-i               Compute index (costs extra memory)" << endl;
+    cerr << "\t-L               Compute and maintain content of skipgrams (costs extra memory)" << endl;
     cerr << "\t-S <number>      Skip type threshold: only skipgrams with x possible types for the skip will be considered, otherwise the skipgram will be pruned  (default: 2, works only with -L enabled)" << endl;
     cerr << "\t-B               Do NOT consider skipgrams that begin with a skip and have no further skips" << endl;
     cerr << "\t-E               Do NOT consider skipgrams that end in a skip and have no further skips" << endl;
@@ -93,6 +93,9 @@ int main( int argc, char *argv[] ) {
         case 'E':
             DOFINALONLYSKIP = false;    
             break;
+        case 'h':
+            usage();
+            exit(0);
         case '?':
             if (optopt == 'c') {
                 cerr <<  "Option -" << optopt << " requires an argument." << endl;
