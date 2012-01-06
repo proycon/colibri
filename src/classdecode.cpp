66fe195b898cecbe7151c4c6fe4ccf13b1827b47
Maarten van Gompel <proycon@anaproy.nl>
2012-01-06 16:11:26 +0100

    made classdecode user-friendly

--- src/classdecode.cpp
+++ src/classdecode.cpp
@@ -1,11 +1,42 @@
 #include <string>
+#include <iostream>
 #include "classdecoder.h"
+#include "getopt.h"
 
 using namespace std;
 
-int main( int argc, char *argv[] ) {
-    const string cls = argv[1];
-    const string clsdec = argv[2];
-    ClassDecoder classdecoder = ClassDecoder(cls);
-    classdecoder.decodefile(clsdec);   
+void usage() {
+    cerr << "Syntax: classdecoder -f encoded-corpus -c class-file" << endl;
+    cerr << "Descriptions: Decodes an encoded corpus" << endl;
+}
+
+int main( int argc, char *argv[] ) {    
+    string classfile = "";
+    string corpusfile = "";
+       
+    char c;    
+    while ((c = getopt(argc, argv, "c:f:h")) != -1)
+        switch (c)
+        {
+        case 'c':
+            classfile = optarg;
+            break;
+        case 'f':
+            corpusfile = optarg;
+            break;        
+        case 'h':
+            usage();
+            exit(0);
+		default:
+            cerr << "Unknown option: -" <<  optopt << endl;
+            abort ();
+        }
+        
+    if (classfile.empty() || corpusfile.empty()) {
+    	usage();
+    	exit(2);
+    }
+    
+    ClassDecoder classdecoder = ClassDecoder(classfile);
+    classdecoder.decodefile(corpusfile);   
 }
