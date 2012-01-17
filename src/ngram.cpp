cdac812c59a6a3ae8dd5b978174f6f30626ed34e
Maarten van Gompel <proycon@anaproy.nl>
2012-01-17 14:29:43 +0100

    fix

--- src/ngram.cpp
+++ src/ngram.cpp
@@ -107,6 +107,10 @@ EncNGram * getencngram(const int index, const int n, const unsigned char *line, 
     int endpos = -1;
     for (int i = 0; i < size; i++) {
         if (line[i] == 0) {
+        	if (i == 0) { //fix for encoding problem: 0 byte at beginning, skip
+        		beginpos = i+1;
+        		continue;
+        	}
             currentindex++;
             if (currentindex == index) {
                 beginpos = i+1;
