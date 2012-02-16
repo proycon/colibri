19311b19b8cce0ebc45486fd03cfeda97f2bbae9
Maarten van Gompel <proycon@anaproy.nl>
2012-02-16 15:04:11 +0100

    output score summary

--- scripts/mtwrapper.py
+++ scripts/mtwrapper.py
@@ -795,7 +795,21 @@ class MTWrapper(object):
                     errors = True    
         else:
             print >>sys.stderr, yellow("Skipping TER (no script found)")     
-     
+    
+        if not errors:
+            f = open(self.WORKDIR + '/summary.score','w')
+            s = "BLEU METEOR NIST TER WER PER"
+            f.write(s+ "\n")
+            print >>sys.stderr, s
+            print >>sys.stderr,"SCORE SUMMARY\n===================\n"
+            s = str(round(self.blue,4)) + " " + str(round(self.meteor,4)) + " " + str(round(self.nist,4)) +  + " " + str(round(self.ter,2)) + " " + str(round(self.wer,2))  + " " + str(round(self.per,2))
+            f.write(s + "\n")
+            print >>sys.stderr, s
+            f.close()
+                        
+        elif os.path.exists(self.WORKDIR + '/summary.score'):
+            os.unlink(self.WORKDIR + '/summary.score')
+                             
         return not errors
     
     def test(self, sourcefile, reffile):
