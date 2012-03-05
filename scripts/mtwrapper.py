c763cc24232e60777dbc5013939f913151e21840
Maarten van Gompel <proycon@anaproy.nl>
2012-03-05 11:47:47 +0100

    fix

--- scripts/mtwrapper.py
+++ scripts/mtwrapper.py
@@ -685,14 +685,14 @@ class MTWrapper(object):
         
         if not self.runcmd(self.EXEC_COLIBRI_PATTERNFINDER + ' -f ' + self.gettargetfilename('clsenc') + ' ' + self.COLIBRI_PATTERNFINDER_OPTIONS, "Building target-side pattern model",self.gettargetfilename('indexedpatternmodel.colibri') ): return False
         
-        if not self.runcmd(self.EXEC_COLIBRI_GRAPHMODEL + ' -f ' + self.getsourcefilename('indexedpatternmodel.colibri') + ' ' + self.COLIBRI_GRAPHMODEL_OPTIONS, "Building source-side graph model",self.getsourcefilename('graphmodel.colibri') ): return False
+        if not self.runcmd(self.EXEC_COLIBRI_GRAPHMODEL + ' -f ' + self.getsourcefilename('indexedpatternmodel.colibri') + ' ' + self.COLIBRI_GRAPHMODEL_OPTIONS, "Building source-side graph model",self.getsourcefilename('graphpatternmodel.colibri') ): return False
 
-        if not self.runcmd(self.EXEC_COLIBRI_GRAPHMODEL + ' -f ' + self.gettargetfilename('indexedpatternmodel.colibri') + ' ' + self.COLIBRI_GRAPHMODEL_OPTIONS, "Building target-side graph model",self.gettargetfilename('graphmodel.colibri') ): return False
+        if not self.runcmd(self.EXEC_COLIBRI_GRAPHMODEL + ' -f ' + self.gettargetfilename('indexedpatternmodel.colibri') + ' ' + self.COLIBRI_GRAPHMODEL_OPTIONS, "Building target-side graph model",self.gettargetfilename('graphpatternmodel.colibri') ): return False
         
         if self.BUILD_COLIBRI_MOSESPHRASETABLE:            
-            if not self.runcmd(self.EXEC_COLIBRI_ALIGNER + ' -s ' + self.getsourcefilename('graphmodel.colibri') + ' -t ' + self.gettargetfilename('graphmodel.colibri') + ' -o ' + self.gets2tfilename() + ' ' + self.COLIBRI_ALIGNER_OPTIONS + ' --simpletable --moses > ' + self.gets2tfilename('phrasetable') , "Building alignment model",self.gets2tfilename('alignmodel.colibri'), self.gets2tfilename('phrasetable') ): return False
+            if not self.runcmd(self.EXEC_COLIBRI_ALIGNER + ' -s ' + self.getsourcefilename('graphpatternmodel.colibri') + ' -t ' + self.gettargetfilename('graphpatternmodel.colibri') + ' -o ' + self.gets2tfilename() + ' ' + self.COLIBRI_ALIGNER_OPTIONS + ' --simpletable --moses > ' + self.gets2tfilename('phrasetable') , "Building alignment model",self.gets2tfilename('alignmodel.colibri'), self.gets2tfilename('phrasetable') ): return False
         else:
-            if not self.runcmd(self.EXEC_COLIBRI_ALIGNER + ' -s ' + self.getsourcefilename('graphmodel.colibri') + ' -t ' + self.gettargetfilename('graphmodel.colibri') + ' -o ' + self.gets2tfilename() + ' ' + self.COLIBRI_ALIGNER_OPTIONS, "Building alignment model",self.gets2tfilename('alignmodel.colibri') ): return False
+            if not self.runcmd(self.EXEC_COLIBRI_ALIGNER + ' -s ' + self.getsourcefilename('graphpatternmodel.colibri') + ' -t ' + self.gettargetfilename('graphpatternmodel.colibri') + ' -o ' + self.gets2tfilename() + ' ' + self.COLIBRI_ALIGNER_OPTIONS, "Building alignment model",self.gets2tfilename('alignmodel.colibri') ): return False
         
         
         
