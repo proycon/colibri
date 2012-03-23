7fb19d2c63a825ca09ea7c911d5dd0eb4bcf6355
Maarten van Gompel <proycon@anaproy.nl>
2012-03-23 21:08:59 +0100

    update

--- src/patternmodel.cpp
+++ src/patternmodel.cpp
@@ -1797,13 +1797,15 @@ void GraphPatternModel::outputrelations( const EncAnyGram * anygram, ostream *OU
 void GraphPatternModel::outputrelations( const unordered_set<const EncAnyGram *> & nodes, ostream *OUT, unordered_map<const EncAnyGram *, unordered_set<const EncAnyGram*> > & relationhash, const std::string & colour) {	
 	for (unordered_set<const EncAnyGram*>::const_iterator iter = nodes.begin(); iter != nodes.end(); iter++) {
 		const EncAnyGram * anygram = model->getkey(*iter);
-		unordered_set<const EncAnyGram*> * relations = &relationhash[anygram];
-	    for (unordered_set<const EncAnyGram*>::iterator iter = relations->begin(); iter != relations->end(); iter++) {
-	    	const EncAnyGram * anygram2  = model->getkey(*iter);
-	    	if (nodes.count(anygram2) > 0) {
-	    		*OUT << "c" << anygram->hash() << " -> " << "c" << anygram2->hash() << " [ color=" << colour << " ];" << endl;
-	    	} 
-	    }				
+		if (relationhash.count(anygram) > 0) {
+			unordered_set<const EncAnyGram*> * relations = &relationhash[anygram];
+			for (unordered_set<const EncAnyGram*>::iterator iter = relations->begin(); iter != relations->end(); iter++) {
+				const EncAnyGram * anygram2  = model->getkey(*iter);
+				if (nodes.count(anygram2) > 0) {
+					*OUT << "c" << anygram->hash() << " -> " << "c" << anygram2->hash() << " [ color=" << colour << " ];" << endl;
+				} 
+			}
+		}				
 	}
 }
 
