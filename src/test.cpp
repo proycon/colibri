0a43aa0883b9fa6e2633df1145ee4824d9ce7bd0
Maarten van Gompel <proycon@anaproy.nl>
2012-03-24 22:48:19 +0100

    test

---
+++ src/test.cpp
@@ -0,0 +1,33 @@
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <patternmodel.h>
+#include <common.h>
+
+
+using namespace std;
+
+int main( int argc, char *argv[] ) {
+	//string model = argv[1];
+	string classfile = argv[1];
+	string querystring = argv[2];
+
+	ClassDecoder classdecoder = ClassDecoder(classfile);
+    ClassEncoder encoder = ClassEncoder(classfile);
+	
+	unsigned char buffer[65536];
+	char buffersize = encoder.encodestring(querystring, buffer);
+    EncNGram ngram = EncNGram(buffer,buffersize-1); //-1 to strip last \0 byte
+    
+    vector<EncNGram*> subngrams;
+    ngram.subngrams(subngrams);
+    for (vector<EncNGram*>::iterator iter2 = subngrams.begin(); iter2 != subngrams.end(); iter2++) {                
+        const EncAnyGram * subngram = *iter2;
+    	subngram->decode(classdecoder);
+    }
+    
+    
+	
+}
