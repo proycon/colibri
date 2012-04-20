73461b0ebd122a0e07f4054135b4cc288bb1c16f
Maarten van Gompel <proycon@anaproy.nl>
2012-04-20 11:31:05 +0200

    fix

--- scripts/mtwrapper.py
+++ scripts/mtwrapper.py
@@ -1634,7 +1634,7 @@ class MTWrapper(object):
         f.write(")\n")
         if writebatches:
             for batch, conf in self.batches:                     
-                f.write("mtwrapper.addbatch('" + batch + "', **"+repr(conf)+")")
+                f.write("mtwrapper.addbatch('" + batch + "', **"+repr(conf)+")\n\n")
         f.write("mtwrapper.start()\n")
         f.close()
         os.chmod(settingsfile, 0754)
