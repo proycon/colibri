4be12bb9cc694eb1445baf3885a497c84e676d1b
Maarten van Gompel <proycon@anaproy.nl>
2012-05-04 12:41:13 +0200

    added statistics

--- scripts/comparephrasetable.py
+++ scripts/comparephrasetable.py
@@ -14,12 +14,14 @@ except:
 pairs = []
 source = []
 target = []
+ptsize = []
 
 for phrasetable in phrasetables:
     pairs.append( defaultdict(int) )
     source.append( defaultdict(int) )
     target.append( defaultdict(int) )
-        
+
+    print >>sys.stderr, "Loading " + phrasetable
     for line in codecs.open(phrasetable,'r','utf-8'):
         fields = line.split('|||')
         s = fields[0].strip()
@@ -27,42 +29,82 @@ for phrasetable in phrasetables:
         pairs[-1][(s,t)] += 1
         source[-1][s] += 1
         target[-1][t] += 1
+        
+
+    
+pairoverlap = 0
+pairtotal = 0
+sourceoverlap = 0
+sourcetotal = 0
+targetoverlap = 0
+targettotal = 0
     
 
+print >>sys.stderr, "Computing pair overlap"
 overlapmem = {}    
-for pair in itertools.chain( ( x.keys() for x in pairs ) ):    
-    overlap = True
-    for i, phrasetable in enumerate(phrasetables): 
-        if not (pair in pairs[i]):
-            overlap = False
-            break
-    if overlap:
-        if not pair in overlapmem:
-            print "PAIR MATCH:", pair
-        overlapmem[pair] = True
-        
-overlapmem = {}        
-for s in  itertools.chain( ( x.keys() for x in source ) ):    
-    overlap = True
-    for i, phrasetable in enumerate(phrasetables): 
-        if not (s in source[i]):
-            overlap = False
-            break
-    if overlap:
-        if not s in overlapmem:
-            print "SOURCE MATCH:", s
-        overlapmem[s] = True
-
+for x in itertools.chain( ( x.keys() for x in pairs ) ):    
+    if not x in overlapmem:
+        overlapmem[x] = True        
+        pairtotal += 1
+        overlap = True
+        for i, phrasetable in enumerate(phrasetables): 
+            if not (x in pairs[i]):
+                overlap = False
+                break
+        if overlap:
+            if not x in overlapmem:                
+                print "PAIR MATCH:", x
+            pairoverlap += 1
+            
+            
+print >>sys.stderr, "Computing source overlap"                    
 overlapmem = {}
-for t in  itertools.chain( ( x.keys() for x in target ) ):    
-    overlap = True
-    for i, phrasetable in enumerate(phrasetables): 
-        if not (t in target[i]):
-            overlap = False
-            break
-    if overlap:
-        if not t in overlapmem:
-            print "TARGET MATCH:",t
-        overlapmem[t] = True
+for x in itertools.chain( ( x.keys() for x in source ) ):    
+    if not x in overlapmem:
+        overlapmem[x] = True        
+        sourcetotal += 1
+        overlap = True
+        for i, phrasetable in enumerate(phrasetables): 
+            if not (x in source[i]):
+                overlap = False
+                break
+        if overlap:
+            if not x in overlapmem:                
+                print "SOURCE MATCH:", x
+            sourceoverlap += 1
+            
+            
+                    
+
+print >>sys.stderr, "Computing target overlap"
+overlapmem = {}
+for x in itertools.chain( ( x.keys() for x in target ) ):    
+    if not x in overlapmem:
+        overlapmem[x] = True        
+        targettotal += 1
+        overlap = True
+        for i, phrasetable in enumerate(phrasetables): 
+            if not (x in target[i]):
+                overlap = False
+                break
+        if overlap:
+            if not x in overlapmem:                
+                print "TARGET MATCH:", x
+            targetoverlap += 1
+            
+
+print >>sys.stderr, "Total combined pairs: " , pairtotal
+for i, phrasetable in enumerate(phrasetables):
+    print >>sys.stderr, "Total pairs in " + phrasetable + ": ", len(pairs[i]), len(pairs[i]) / float(pairtotal)
+print >>sys.stderr, "Overlapping pairs: " , pairoverlap,  pairoverlap / float(pairtotal)
+
+print >>sys.stderr, "Total combined source patterns: " , sourcetotal
+for i, phrasetable in enumerate(phrasetables):
+    print >>sys.stderr, "Total source patterns in " + phrasetable + ": ", len(source[i]), len(source[i]) / float(sourcetotal)
+print >>sys.stderr, "Overlapping source patterns: " , sourceoverlap,  sourceoverlap / float(sourcetotal)
 
 
+print >>sys.stderr, "Total combined target patterns: " , targettotal
+for i, phrasetable in enumerate(phrasetables):
+    print >>sys.stderr, "Total target patterns in " + phrasetable + ": ", len(target[i]), len(target[i]) / float(targettotal)
+print >>sys.stderr, "Overlapping target patterns: " , targetoverlap,  targetoverlap / float(targettotal)
