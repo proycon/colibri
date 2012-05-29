1df1ac716d2f4d3dff9986f8c55e6a0bb59ba40b
Maarten van Gompel <proycon@anaproy.nl>
2012-05-29 12:15:18 +0200

    fix

--- scripts/intersectphrasetables.py
+++ scripts/intersectphrasetables.py
@@ -54,7 +54,7 @@ def computeoverlap(items, label= 'PAIR', pivot = 0):
                     overlaps = False
                     break
             if overlaps:      
-                s = item[0] + ' ||| ' + item[1] + ' ||| ' + scores[item] 
+                s = item[0] + ' ||| ' + item[1] + ' ||| ' + ' '.join(scores[item]) 
                 print s.encode('utf-8')
                 overlap += 1
             overlapmem[item] = overlap 
