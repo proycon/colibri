31ed2df69a5dc5d8e99c6005616da26dd43d38a7
Maarten van Gompel <proycon@anaproy.nl>
2012-10-05 17:06:54 +0200

    debug

--- include/ngram.h
+++ include/ngram.h
@@ -137,7 +137,7 @@ class EncSkipGram: public EncAnyGram {
       EncSkipGram(const unsigned char *dataref, const char size, const unsigned char* skipref, const char skipcount);
       //EncSkipGram(const EncNGram & pregap, const EncNGram & postgap, const char refn);
       EncSkipGram(std::istream * in, const char gapcount = -1);
-      std::string decode(ClassDecoder& classdecoder) const;
+      virtual std::string decode(ClassDecoder& classdecoder) const;
       bool out() const;
       bool isskipgram() const { return (skipcount > 0); }
       
