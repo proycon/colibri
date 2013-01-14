9cfa5dd9e589798a80bb18d6f6962e30911c3041
Maarten van Gompel <proycon@anaproy.nl>
2013-01-14 21:50:31 +0100

    debug

--- src/alignmodel.cpp
+++ src/alignmodel.cpp
@@ -2184,7 +2184,7 @@ void AlignmentModel::load(const string & filename, bool logprobs, bool allowskip
         if (gapcount == 0) {
             if (DEBUG)  cerr << "\tNGRAM";
             const EncNGram * ngram = new EncNGram(&f, ngramversion); //read from file
-            if (DEBUG)  cerr << " n=" << ngram->n() << " size=" << ngram->size();
+            if (DEBUG)  cerr << " n=" << (int) ngram->n() << " size=" << (int) ngram->size();
             sourcegram = getsourcekey((EncAnyGram*) ngram); //does the key already exist?
             if (sourcegram == NULL) {            
                 //no
@@ -2237,7 +2237,7 @@ void AlignmentModel::load(const string & filename, bool logprobs, bool allowskip
 		    if (gapcount == 0) {
 		        if (DEBUG)  cerr << "\tNGRAM";
 		        EncNGram ngram = EncNGram(&f, ngramversion); //read from file
-		        if (DEBUG)  cerr << " n=" << ngram.n() << " size=" << ngram.size();
+		        if (DEBUG)  cerr << " n=" << (int) ngram.n() << " size=" << (int) ngram.size();
 		        if (!gettargetkey((EncAnyGram*) &ngram)) {
 		        	targetngrams.insert(ngram);		        	
 		        }   
