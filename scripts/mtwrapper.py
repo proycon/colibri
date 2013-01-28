f48d814da364fd5b6d5d78ba6e2c17526788e9fe
Maarten van Gompel <proycon@anaproy.nl>
2013-01-28 14:02:33 +0100

    pass target corpus to contextmoses as well

--- scripts/mtwrapper.py
+++ scripts/mtwrapper.py
@@ -1814,7 +1814,7 @@ class MTWrapper(object):
         
         if not self.runcmd(self.EXEC_COLIBRI_CLASSENCODE + ' -f ' + self.gettargetfilename('txt'), "Encoding target corpus for Colibri",self.gettargetfilename('cls'), self.gettargetfilename('clsenc') ): return False        
         
-        if not self.runcmd(self.EXEC_COLIBRI_CONTEXTMOSES + ' -f ' + self.getsourcefilename('clsenc') + ' -m ' +  self.gets2tfilename('phrasetable') + ' -S ' +  self.getsourcefilename('cls') + ' -T ' + self.gettargetfilename('cls') + ' -l ' + str(self.MOSES_LEFTCONTEXTSIZE) + ' -r ' + str(self.MOSES_RIGHTCONTEXTSIZE) + ' ' + self.MOSES_CLASSIFIER_OPTIONS, "Training classifiers for context-aware moses"): return False        
+        if not self.runcmd(self.EXEC_COLIBRI_CONTEXTMOSES + ' -f ' + self.getsourcefilename('clsenc') + ' -g ' + self.gettargetfilename('clsenc') + ' -m ' +  self.gets2tfilename('phrasetable') + ' -S ' +  self.getsourcefilename('cls') + ' -T ' + self.gettargetfilename('cls') + ' -l ' + str(self.MOSES_LEFTCONTEXTSIZE) + ' -r ' + str(self.MOSES_RIGHTCONTEXTSIZE) + ' ' + self.MOSES_CLASSIFIER_OPTIONS, "Training classifiers for context-aware moses"): return False        
         
         return True
     
