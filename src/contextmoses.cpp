b95c6a23bb8994149d1a1a41a48a3892171dc557
Maarten van Gompel <proycon@anaproy.nl>
2013-04-22 10:31:44 +0200

    update

--- src/contextmoses.cpp
+++ src/contextmoses.cpp
@@ -472,9 +472,6 @@ int main( int argc, char *argv[] ) {
                 throw InternalError();
             }
 
-            if ((DOKEYWORDS) && (contextalignmodel->keywords.empty())) {
-                cerr << "WARNING: No keywords available in alignment model" << endl;
-            }
 
                                     
             if (linesize > 0) {
