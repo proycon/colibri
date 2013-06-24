4497dc20eb3a383138c1e5ec5950aabe6a8e5895
Maarten van Gompel <proycon@anaproy.nl>
2013-06-25 00:11:55 +0200

    debug

--- pycolibri_wrapper.pyx
+++ pycolibri_wrapper.pyx
@@ -64,12 +64,15 @@ cdef class IndexedPatternModel:
         #TODO: skipgrams
 
     def reverseindex(self, int index):
-        cdef vector[const pycolibri_classes.EncAnyGram *] v = self.thisptr.reverse_index[index]
-        #cdef vector[const pycolibri_classes.EncAnyGram*] v = self.thisptr.get_reverse_index(index)
+        #cdef vector[const pycolibri_classes.EncAnyGram *] v = self.thisptr.reverse_index[index]
+        cdef vector[const pycolibri_classes.EncAnyGram*] v = self.thisptr.get_reverse_index(index)
+        print("got v")
         cdef vector[const pycolibri_classes.EncAnyGram *].iterator it = v.begin()
         while it != v.end():
             anygram  = <pycolibri_classes.EncAnyGram*> address(deref(it))
+            print("got anygram")
             anygram.decode(deref(self.decoder.thisptr))
+            print("decoded")
             pattern = Pattern()
             pattern.bind(anygram)
             yield pattern
